715b846fb7cfaf7096d205bf1466cd090e8ae85a
dazzlemon <30831873+dazzlemon@users.noreply.github.com>
2022-04-25 19:06:57 +0300

    bring back support for java swing gui apps

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,16 +1,16 @@
 import XMonad
 import XMonad.Hooks.Rescreen
+import XMonad.Hooks.SetWMName
 
 myRandrChangeHook :: X ()
 myRandrChangeHook = spawn "xrandr --auto"
 
-rescreenCfg = def 
-	{ randrChangeHook = myRandrChangeHook
-	}
+rescreenCfg = def { randrChangeHook = myRandrChangeHook }
 
 main = xmonad
      $ rescreenHook rescreenCfg 
-		 $ def
-      	{ terminal	= "st"
-      	, modMask	= mod4Mask
-      	}
+     $ def { terminal = "st"
+           , modMask  = mod4Mask
+           , startupHook = do
+               setWMName "LG3D" -- support for java swing gui apps
+           }
